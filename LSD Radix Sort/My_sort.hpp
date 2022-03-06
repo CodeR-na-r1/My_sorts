88c7edf301ddc76cd8b1087721994dd3835459b5
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2022-03-06 23:16:36 +0800

    Эскиз

---
+++ LSD Radix Sort/My_sort.hpp
@@ -0,0 +1,34 @@
+﻿namespace My_sort
+{
+	class Sort
+	{
+	public:
+
+		template<typename T>
+		static void LSD_sort(T& data, const size_t size, const int count_value_digit = 256, const int offset = 8)
+		{
+			int count_bytes = sizeof(T);
+
+			for (size_t i = 0; i < count_bytes; i++)
+			{
+				int indexes[count_value_digit] = { 0 };	// 
+				for (size_t j = 0; j < size; j++)
+				{
+					++indexes[*(unsigned char*)&(data[j] >> (i * offset))];	// Возможно мы приводим не то (временный объект мэйби)
+				}
+
+				for (size_t j = 1; j < count_value_digit; j++)
+				{
+					indexes[j] += indexes[j - 1];	// Аккумулируем индексы для адресации в дальнейшем
+				}
+
+				for (size_t j = 1; j < count_value_digit; j++)
+				{
+						// 
+				}
+			}
+
+			return;
+		}
+	};
+}
---
+++ LSD Radix Sort/Source.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#include "Generate_number.hpp"
+
+#include "My_sort.hpp"
+using namespace My_sort;
+
+using namespace std;
+
+#define SIZE 1'000//'000
+
+int main()
+{
+	int* arr = new int[SIZE];
+
+	unique_gen(arr, 0, SIZE);
+
+	Sort::LSD_sort(arr, SIZE);
+
+	delete[] arr;
+
+	return 0;
+}
