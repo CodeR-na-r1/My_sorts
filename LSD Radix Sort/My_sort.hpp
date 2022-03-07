5b13ad7939024739f3f386deccf4c6190c4668fe
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2022-03-07 23:34:37 +0800

    Работает!

--- LSD Radix Sort/My_sort.hpp
+++ LSD Radix Sort/My_sort.hpp
@@ -8,23 +8,24 @@ namespace My_sort
 	public:
 
 		template<typename T, typename Type_iterator>
+		// 
 		static void LSD_sort(T& data, const Type_iterator size, const int count_value_digit = 256, const int offset = 8)
 		{
-			int* data_supporting = new int[size];
+			int* data_supporting = new int[size];	// Вспомогательный контейнер для записи сюда значений на этапах сортировки (для каждого разраяда)
 
-			int* helper_exchange;
+			int* helper_exchange;	// Указатель для упрощения размена указателей (позволяет не делать копирование)
 
-			int count_bytes = sizeof(T);
+			int count_bytes = sizeof(T);	// Количество байт, занимаемое единицой коллекции (определяет количество этапов сортировки)
 
-			int intermediate;
+			int intermediate;	// Переменная для промежуточных даннх в процессе вычислений
 
-			for (Type_iterator i = 0; i < count_bytes; i++)
+			for (Type_iterator i = 0; i < count_bytes; i++)	// Цикл по кол-ву этапов сортировки
 			{
-				int indexes[256] = { 0 };	// вспомогательный массив	// [count_value_digit]
+				int indexes[256] = { 0 };		// вспомогательный массив
 				for (Type_iterator j = 0; j < size; j++)
 				{
-					intermediate = data[j] >> (i * offset);
-					++indexes[*(unsigned char*)&intermediate];	// Возможно мы приводим не то (временный объект мэйби)
+					intermediate = data[j] >> (i * offset);		// Берём нужный для данного этапа кусок данных (по дефолту - 1 байт)
+					++indexes[*(unsigned char*)&intermediate];	// Инкреминтируем данное значение
 				}
 
 				for (Type_iterator j = 1; j < count_value_digit; j++)
@@ -35,23 +36,18 @@ namespace My_sort
 				for (signed int j = size - 1; j >= 0; j--)
 				{
 					intermediate = data[j] >> (i * offset);
-					auto x = indexes[*(unsigned char*)&intermediate] - 1;
-					auto two = data[j];
+					//auto x = indexes[*(unsigned char*)&intermediate] - 1;	// debug use only
+					//auto two = data[j];	// debug use only
 					data_supporting[indexes[*(unsigned char*)&intermediate]-- - 1] = data[j];		// Сортируем по данному разряду
 				}
 
 				// Для избавления от копирования массивов, размениваем указатели
 				helper_exchange = data;
-				data = data_supporting;
-				data_supporting = helper_exchange;
-
-				cout << endl << i << endl;
-				for (size_t i = 0; i < size; i++)
-				{
-					cout << data[i] << " ";
-				}
+				data = data_supporting;		// В указатель для контейнера с основными данными забираем актуальные данные
+				data_supporting = helper_exchange;	// В указатель для вспомогательного контейнера забираем уже неактуальные данные
 			}
 
+			// Так как указатели на одну область память указывают, то:
 			helper_exchange = nullptr;
 			delete[] data_supporting;
 
--- LSD Radix Sort/Source.cpp
+++ LSD Radix Sort/Source.cpp
@@ -1,35 +1,60 @@
 ﻿#include <iostream>
 
-#include "Generate_number.h"
+#include "Mytime.hpp"	// Для замера времени выполнения участка кода с сортировкой
 
-#include "My_sort.h"
+#include "Generate_number.hpp"	// Для генерации неповторяющихся случайных чисел
+
+#include "My_sort.hpp"	// Тут версия алгоритма LSD
 using namespace My_sort;
 
 using namespace std;
 
-#define SIZE 8
+#define SIZE 52'428'800ULL	// Количество элементов в коллекции для сортировки
+
+void print_arr(int* arr, int size)	// Функция для выввода массива
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int cmp(const void* one, const void* two)	// Аля типо функтор для std::qsort
+{
+	return *(int*)one > *(int*)two ? 1 : -1;
+}
 
 int main()
 {
-	int* arr = new int[SIZE];
+	int* data = new int[SIZE];	// Основной контейнер с неповторяющиися рандомными числами
+	int* arr = new int[SIZE];	// Вспомогательный контейнер
 
-	unique_gen(arr, 0, SIZE, 10000);
+	unique_gen(data, 0ULL, SIZE);	// Генерация неповторяющихся случайных чисел
 
-	cout << endl;
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << " ";
-	}
+	// ----------------- LSD_sort -------------------------
+
+	std::copy(data, data + SIZE, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	Mytime time;	// Замер времени
+	My_sort::Sort::LSD_sort(arr, SIZE);	// LSD_sort
+	cout << "LSD Sort time -> \t" << time.get_time() << endl;	// Выввод времени
+
+	// ----------------- std::qsort -------------------------
+
+	std::copy(data, data + SIZE - 1, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	time.retime();	// Сброс времени
+	std::qsort(arr, SIZE, sizeof(int), cmp);	// std::qsort
+	cout << "qsort Sort time -> \t" << time.get_time() << endl;
+
+	// ----------------- std::sort -------------------------
+
+	std::copy(data, data + SIZE - 1, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	time.retime();
+	std::sort(arr, arr + SIZE);	// std::sort
+	cout << "sort Sort time -> \t" << time.get_time() << endl;
 	
-	My_sort::Sort::LSD_sort(arr, SIZE);
-	
-	cout << endl;
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	
-	delete[] arr;
+	delete[] arr;	// Чистим данные
+	delete[] data;
 
 	return 0;
 }
