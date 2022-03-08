b9d8308d0771e5733cf5c8624a8383679d8af62a
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2022-03-08 17:37:01 +0800

    Версия для защиты на проекте. с 0xFF медленнее

---
+++ LSD Radix Sort/surrendered version/My_sort.hpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+namespace My_sort
+{
+	class Sort
+	{
+	public:
+
+		template<typename T>
+		static void LSD_sort(T& data, const unsigned int size, const int count_value_digit = 256, const int offset = 8)
+		{
+			int* data_supporting = new int[size];	// Вспомогательный контейнер для записи сюда значений на этапах сортировки (для каждого разраяда)
+
+			int* helper_exchange;	// Указатель для упрощения размена указателей (позволяет не делать копирование)
+
+			int count_bytes = sizeof(T);	// Количество байт, занимаемое единицой коллекции (определяет количество этапов сортировки)
+
+			int intermediate;	// Переменная для промежуточных даннх в процессе вычислений
+
+			for (unsigned int i = 0; i < count_bytes; i++)	// Цикл по кол-ву этапов сортировки
+			{
+				int indexes[256] = { 0 };		// вспомогательный массив
+				for (unsigned int j = 0; j < size; j++)
+				{
+					intermediate = data[j] >> (i * offset);		// Берём нужный для данного этапа кусок данных (по дефолту - 1 байт)
+					++indexes[intermediate & 0x000000FF];	// Инкреминтируем данное значение
+				}
+
+				for (unsigned int j = 1; j < count_value_digit; j++)
+				{
+					indexes[j] += indexes[j - 1];	// Аккумулируем индексы для адресации в дальнейшем
+				}
+
+				for (signed int j = size - 1; j >= 0; j--)
+				{
+					intermediate = data[j] >> (i * offset);
+					data_supporting[indexes[intermediate & 0x000000FF]-- - 1] = data[j];		// Сортируем по данному разряду
+				}
+
+				// Для избавления от копирования массивов, размениваем указатели
+				helper_exchange = data;
+				data = data_supporting;		// В указатель для контейнера с основными данными забираем актуальные данные
+				data_supporting = helper_exchange;	// В указатель для вспомогательного контейнера забираем уже неактуальные данные
+			}
+
+			// Так как указатели на одну область память указывают, то:
+			helper_exchange = nullptr;
+			delete[] data_supporting;
+
+			return;
+		}
+	};
+}
---
+++ LSD Radix Sort/surrendered version/Source.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <fstream>
+
+#include "Mytime.hpp"	// Для замера времени выполнения участка кода с сортировкой
+
+#include "Generate_number.hpp"	// Для генерации неповторяющихся случайных чисел
+
+#include "My_sort.hpp"	// Тут версия алгоритма LSD
+using namespace My_sort;
+
+using namespace std;
+
+#define SIZE 5000000ULL	// Количество элементов в коллекции для сортировки
+
+void print_arr(int* arr, int size)	// Функция для выввода массива
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int cmp(const void* one, const void* two)	// Аля типо функтор для std::qsort
+{
+	return *(int*)one >= *(int*)two ? 1 : -1;
+}
+
+int main()
+{
+	ofstream file("output.data", ios::app);
+	file << SIZE << " ";
+	float time_temp;
+
+	int* data = new int[SIZE];	// Основной контейнер с неповторяющиися рандомными числами
+	int* arr = new int[SIZE];	// Вспомогательный контейнер
+
+	unique_gen(data, 0ULL, SIZE);	// Генерация неповторяющихся случайных чисел
+
+	// ----------------- LSD_sort -------------------------
+
+	std::copy(data, data + SIZE, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	Mytime time;	// Замер времени
+	My_sort::Sort::LSD_sort(arr, SIZE);	// LSD_sort
+	time_temp = time.get_time();
+	cout << "LSD Sort time -> \t" << time_temp << endl;	// Выввод времени
+	file << time_temp << " ";
+
+	// ----------------- std::qsort -------------------------
+
+	std::copy(data, data + SIZE - 1, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	time.retime();	// Сброс времени
+	std::qsort(arr, SIZE, sizeof(int), cmp);	// std::qsort
+	time_temp = time.get_time();
+	cout << "qsort Sort time -> \t" << time_temp << endl;
+	file << time_temp << " ";
+
+	// ----------------- std::sort -------------------------
+
+	std::copy(data, data + SIZE - 1, arr);	// Копируем данные во вспомогательный контейнер для передачи следующему алгоритму сортировки
+	time.retime();
+	std::sort(arr, arr + SIZE);	// std::sort
+	time_temp = time.get_time();
+	cout << "sort Sort time -> \t" << time_temp << endl;
+	file << time_temp << endl;
+	
+	delete[] arr;	// Чистим данные
+	delete[] data;
+
+	file.close();
+
+	return 0;
+}
